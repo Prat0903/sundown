78a2c62f02e8597c91f3f0f366b8907d5be6c196
Prateek Kumar <wckprateek61@gmail.com>
2025-01-05 00:45:03 +0530

    Responsive mostly done and yet to do few changes

--- script.js
+++ script.js
@@ -4,24 +4,45 @@ const scroll = new LocomotiveScroll({
 });
 
 function hoverAnimation() {
-    var elemContainer = document.querySelector('#elem-container')
-    var fixed = document.querySelector('#fixed-image');
+    const elems = document.querySelectorAll('.elem');
+    const fixed = document.querySelector('#fixed-image');
 
-    elemContainer.addEventListener('mouseenter', function () {
-        fixed.style.display = 'block';
-    })
+    function applyLayout() {
+        elems.forEach((elem) => {
+            const image = elem.getAttribute('data-image');
 
-    elemContainer.addEventListener('mouseleave', function () {
-        fixed.style.display = 'none';
-    })
+            if (window.innerWidth > 600) {
+                const imgElement = elem.querySelector('img');
+                if (imgElement) {
+                    imgElement.remove();
+                }
+                elem.addEventListener('mouseenter', () => {
+                    fixed.style.backgroundImage = `url(${image})`;
+                    fixed.style.display = 'block';
+                });
+                elem.addEventListener('mouseleave', () => {
+                    fixed.style.display = 'none';
+                });
+            } else {
 
-    var elems = document.querySelectorAll('.elem');
-    elems.forEach((elem) => {
-        elem.addEventListener('mouseenter', function () {
-            var image = elem.getAttribute('data-image')
-            fixed.style.backgroundImage = `url(${image})`;
-        })
-    })
+                if (!elem.querySelector('img')) {
+                    const imgElement = document.createElement('img');
+                    imgElement.src = image;
+                    imgElement.alt = '';
+                    elem.insertBefore(imgElement, elem.firstChild);
+                }
+
+                elem.removeEventListener('mouseenter', () => { });
+                elem.removeEventListener('mouseleave', () => { });
+            }
+        });
+    }
+
+    applyLayout();
+
+    window.addEventListener('resize', () => {
+        applyLayout();
+    });
 }
 
 function loaderAnimation() {
@@ -40,6 +61,34 @@ function swiperAnimation() {
     });
 }
 
+function menuAnimation() {
+    var menuicon = document.querySelector('#menu i');
+    var full = document.querySelector('#full-scr');
+    var navimg = document.querySelector('nav img');
+    var flag = 0;
+
+    menuicon.addEventListener('click', function () {
+        if (flag == 0) {
+            full.style.top = '0'
+            navimg.style.opacity = 0;
+            menuicon.classList.remove('ri-menu-line');
+            menuicon.classList.add('ri-close-large-fill');
+            menuicon.style.transform = 'rotate(180deg)'
+            menuicon.style.transition = 'all ease 0.3s'
+            flag = 1
+        } else {
+            full.style.top = '-100%';
+            navimg.style.opacity = 1;
+            menuicon.classList.remove('ri-close-large-fill');
+            menuicon.classList.add('ri-menu-line');
+            menuicon.style.transform = 'rotate(0deg)'
+            menuicon.style.transition = 'all ease 0.3s'
+            flag = 0;
+        }
+    })
+}
+
 hoverAnimation();
 loaderAnimation();
 swiperAnimation();
+menuAnimation();
