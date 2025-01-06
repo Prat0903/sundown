026739e8dc2fb356d50e6ef3a5e07eb595522023
Prateek Kumar <wckprateek61@gmail.com>
2025-01-07 01:11:34 +0530

    An inefficient approach

--- script.js
+++ script.js
@@ -88,7 +88,49 @@ function menuAnimation() {
     })
 }
 
+function toggle() {
+    var design = document.querySelector('#design');
+    var project = document.querySelector('#project');
+    var execution = document.querySelector('#execution');
+    var description = document.querySelector('#description');
+    var cover = document.querySelector('#cover-image');
+
+    design.addEventListener('click', function () {
+        description.innerHTML = '<p>Our design team is your first point of contact for any project. They create eye-catching, high-quality visuals that inspire and captivate your customers. We work closely with architects, interior designers, and other relevant stakeholders to ensure a seamless and memorable experience.</p>';
+        cover.src = 'https://sundown-ivory.vercel.app/page4-1.webp';
+        design.style.color = 'white';
+        design.style.marginLeft = 0;
+        project.style.marginLeft = '2vw';
+        project.style.color = '#504a45';
+        execution.style.marginLeft = '2vw';
+        execution.style.color = '#504a45';
+    })
+
+    project.addEventListener('click', function () {
+        description.innerHTML = '<p>Once we have a design, our production team takes the lead in bringing it to life. We manage all stages of the project, from build specifications and technical drawings to site surveys, vendor management, and 2D & 3D production. We have an extensive network of partners to meet each unique design and project need.</p>';
+        cover.src = 'https://sundown-ivory.vercel.app/page4-2.webp';
+        project.style.color = 'white';
+        project.style.marginLeft = 0;
+        design.style.marginLeft = '2vw';
+        design.style.color = '#504a45';
+        execution.style.marginLeft = '2vw';
+        execution.style.color = '#504a45';
+    })
+
+    execution.addEventListener('click', function () {
+        description.innerHTML = '<p>We’re with you every step of the way, from the design initiation to launch day. Our production and design teams are onsite to direct and guide the process down to the last point of completion, ensuring success across the built space and experience.</p>';
+        cover.src = 'https://sundown-ivory.vercel.app/page4-3.webp';
+        execution.style.color = 'white';
+        execution.style.marginLeft = 0;
+        design.style.marginLeft = '2vw';
+        design.style.color = '#504a45';
+        project.style.marginLeft = '2vw';
+        project.style.color = '#504a45';
+    })
+}
+
 hoverAnimation();
 loaderAnimation();
 swiperAnimation();
 menuAnimation();
+toggle();
